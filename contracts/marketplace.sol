974e2ee4b497bd3889267337b588fa322ce918a1
Thomas Carl <n0identity1013@hotmail.com>
2021-08-05 12:14:09 +0200

    ERC1155

---
+++ contracts/marketplace.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.5.0;
+
+import "../lib/IERC1155.sol";
+
+contract Marketplace {
+    IERC1155 private _token;
+
+    mapping (uint256 => uint256) price;
+
+    constructor(IERC1155 token) public {
+      require(address(token) != address(0));
+      _token = token;
+      price[1] = 100000000000000;
+      price[2] = 100000000000000;
+      price[3] = 100000000000000;
+    }
+
+    function() external payable{
+      buyToken(1);
+    }
+
+    function buyToken(uint256 tokenId) public payable{
+      uint256 weiAmount = msg.value;
+      require(weiAmount >= price[tokenId] && price[tokenId] != 0);
+
+      _token.safeTransferFrom(address(this), msg.sender, tokenId, 1, "");
+
+    }
+
+    function onERC1155Received(address _operator, address _from, uint256 _id, uint256 _value, bytes calldata _data) external returns(bytes4){
+        return bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)"));
+    }
+
+
+
+}
---
+++ migrations/2_token_migrations.js
@@ -0,0 +1,9 @@
+var Token = artifacts.require("./GameToken.sol");
+
+module.exports = (deployer) => deployer
+  .then( () => deployToken(deployer));
+
+
+function deployToken(deployer){
+  return deployer.deploy(Token);
+}
